b09fe97b63283b73696c49a8f45efc5f9c5a6f69
skmkqw <tkorsakov77@gmail.com>
2024-12-13 22:23:55 +0100

    BackendDbContext.cs

---
+++ HackathonBackend.Infrastructure/Persistence/BackendDbContext.cs
@@ -0,0 +1,23 @@
+using HackathonBackend.Domain.Common.Attributes;
+using HackathonBackend.Domain.UserAggregate;
+using HackathonBackend.Infrastructure.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace HackathonBackend.Infrastructure.Persistence;
+
+public class BackendDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    
+    public BackendDbContext(DbContextOptions<BackendDbContext> options) : base(options)
+    {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(BackendDbContext).Assembly);
+        
+        modelBuilder.AddStronglyTypedIdValueConverters<EfCoreValueConverterAttribute>();
+    }
+}
